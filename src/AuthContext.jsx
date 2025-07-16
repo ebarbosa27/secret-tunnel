456984f40ba4a047d18248c9c105337158b6fb4c
ebarbosa27 <ebarbosa5449@gmail.com>
2025-07-15 23:42:44 -0500

    developed app to create a token for the user and authenticate user

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -9,10 +9,46 @@ export function AuthProvider({ children }) {
   const [location, setLocation] = useState("GATE");
 
   // TODO: signup
+  async function signup(username) {
+    try {
+      const response = await fetch(API + "/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+        }),
+      });
+      const resJson = await response.json();
+      if (!resJson.success) throw Error(response);
+      setToken(resJson.token);
+      setLocation("TABLET");
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   // TODO: authenticate
+  async function authenticate() {
+    try {
+      if (!token) throw Error("No token found.");
+      const response = await fetch(API + "/authenticate", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const resJson = await response.json();
+      if (!resJson.success) throw Error(response);
+      setLocation("Tunnel");
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
-  const value = { location };
+  const value = { location, signup, authenticate };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
